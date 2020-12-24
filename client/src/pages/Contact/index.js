7b7ab8abcff8c92126a853a93374a91652f6eb7d
Steffano <smalagadev@gmail.com>
2020-12-24 02:34:12 -0500

    Page mobile responsive

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,41 +1,47 @@
 import './index.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { IconContext } from "react-icons";
+import { RiMailLine, RiSmartphoneLine, RiGithubLine, RiLinkedinBoxLine } from 'react-icons/ri';
 
 function Contact(){
   const subject = "We Want to Meet You!";
 
   return(
     <Card>
-      <Card.Img variant="top" src="assets/images/profile.jpg" />
-      <Card.Body>
-        <Card.Title>Contact Me</Card.Title>
-        <Card.Text>
-          <p>
-            <i className="bi bi-envelope"> </i>YOU CAN CONTACT MY EMAIL:
-            <a target="_blank" href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=smalagadev@gmail.com&su=${subject}&tf=1`}>
-              SMALAGADEV@GMAIL.COM
-            </a>
-          </p>
-
-          <p>
-            <i className="bi bi-telephone"></i>
-            YOU CAN REACH ME AT: <a href="tel:9543192000">9543192000</a>
-          </p>
-          <p>YOU CAN ALSO FIND ME ON THESE PLATFORMS:</p>
-          <ul className="list-inline" id="social-media">
-            <li>
-              <i className="bi bi-github"></i>
-              <a href="https://github.com/smalagadev">GITHUB</a>
-            </li>
-            <li>
-              <i className="bi bi-linkedin"></i>
-              <a href="https://linkedin.com/in/smalagadev">LINKEDIN</a>
-            </li>
-          </ul>
-        </Card.Text>
-        <Button variant="Make ya move"></Button>
-      </Card.Body>
+      <Row>
+      <Col lg={4} md={12} sm={12} xs={12}>
+        <Card.Img variant="top" src="assets/images/profile.jpg" />
+      </Col>
+      <Col lg={8} md={12} sm={12} xs={12}>
+        <Card.Body>
+          <Card.Title>Contact Me</Card.Title>
+          <Card.Text>
+            <p>
+              YOU CAN CONTACT MY EMAIL <RiMailLine/>:
+              <a target="_blank" href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=smalagadev@gmail.com&su=${subject}&tf=1`}>
+                SMALAGADEV@GMAIL.COM
+              </a>
+            </p>
+            <p>
+              YOU CAN REACH ME AT <RiSmartphoneLine/>: <a href="tel:9543192000">9543192000</a>
+            </p>
+            <p>YOU CAN ALSO FIND ME ON THESE PLATFORMS:</p>
+            <ListGroup horizontal>
+              <ListGroup.Item>
+                  <a targer="_blank" href="https://github.com/smalagadev"><RiGithubLine/>GITHUB</a>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                  <a targer="_blank" href="https://linkedin.com/in/smalagadev"><RiLinkedinBoxLine/>LINKEDIN</a>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card.Text>
+        </Card.Body>
+      </Col>
+      </Row>
     </Card>
   );
 }
