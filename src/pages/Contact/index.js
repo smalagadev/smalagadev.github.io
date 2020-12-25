e83be49b8ff8146fcc246d0c9a123a0d0c695ad8
Steffano <smalagadev@gmail.com>
2020-12-25 03:55:29 -0500

    Card.Text rewritten, new Card.Body added

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,15 +3,14 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
 import { IconContext } from "react-icons";
-import { RiMailLine, RiSmartphoneLine, RiGithubLine, RiLinkedinBoxLine } from 'react-icons/ri';
+import { RiMailLine, RiSmartphoneLine, RiGithubLine, RiLinkedinBoxLine, RiFileDownloadLine } from 'react-icons/ri';
 
 function Contact(){
   const subject = "We Want to Meet You!";
 
   return(
-    <Card>
+    <Card id="contact">
       <Row>
       <Col lg={4} md={12} sm={12} xs={12}>
         <Card.Img variant="top" src="assets/images/profile.jpg" />
@@ -21,25 +20,23 @@ function Contact(){
           <Card.Title>Contact Me</Card.Title>
           <Card.Text>
             <p>
-              YOU CAN CONTACT MY EMAIL <RiMailLine/>:
+              <RiMailLine/> You can contact my email:
               <a target="_blank" href={`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=smalagadev@gmail.com&su=${subject}&tf=1`}>
                 SMALAGADEV@GMAIL.COM
               </a>
+              <br/>
+              <RiSmartphoneLine/> You can reach me at: <a href="tel:9543192000">9543192000</a>
+              <br/>
+              <RiFileDownloadLine/> Download my <a href="assets/Resume_Steffano_Malaga.pdf" download>resumé</a>.
             </p>
-            <p>
-              YOU CAN REACH ME AT <RiSmartphoneLine/>: <a href="tel:9543192000">9543192000</a>
-            </p>
-            <p>YOU CAN ALSO FIND ME ON THESE PLATFORMS:</p>
-            <ListGroup horizontal>
-              <ListGroup.Item>
-                  <a targer="_blank" href="https://github.com/smalagadev"><RiGithubLine/>GITHUB</a>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                  <a targer="_blank" href="https://linkedin.com/in/smalagadev"><RiLinkedinBoxLine/>LINKEDIN</a>
-              </ListGroup.Item>
-            </ListGroup>
           </Card.Text>
         </Card.Body>
+        <Card.Body>
+          <p>You can also find me on these platforms:</p>
+          <Card.Link target="_blank" href="https://github.com/smalagadev"><RiGithubLine/>GITHUB</Card.Link>
+          <Card.Link target="_blank" href="https://linkedin.com/in/smalagadev"><RiLinkedinBoxLine/>LINKEDIN</Card.Link>
+        </Card.Body>
+
       </Col>
       </Row>
     </Card>
