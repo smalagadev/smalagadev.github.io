82a5871a372c1a536765247981c7220c85373a0b
Steffano <smalagadev@gmail.com>
2020-12-25 04:00:33 -0500

    Project Table swapped for card columns

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,10 +1,11 @@
 import ProjectCardColumns from './../../components/ProjectCardColumns';
 
-function Projects(){
+export default function Projects(){
   const PROJECTS =
     [
       {
        name:"Word Guess Game",
+       preview: "./assets/preview_images/SkatevideoGuessingGame.png",
        description: "A word guessing game made of JavaScript with Skate video theme.",
        link: "https://smalagadev.github.io/Word-Guess-Game/",
        github: "https://github.com/smalagadev/Word-Guess-Game"
@@ -12,6 +13,7 @@ function Projects(){
 
      {
        name:"Star Wars RPG",
+       preview: "./assets/preview_images/RogueSquadronRPG.png",
        description: "A Star Wars RPG made of JavaScript and JQuery.",
        link: "https://smalagadev.github.io/Rogue-Squadron-RPG/",
        github: "https://github.com/smalagadev/Rogue-Squadron-RPG"
@@ -19,6 +21,7 @@ function Projects(){
 
      {
         name:"Timed Quiz",
+        preview: "./assets/preview_images/TimedQuiz.png",
         description: "A timed quiz made of JavaScript.",
         link: "https://smalagadev.github.io/Timed-Quiz/",
         github: "https://github.com/smalagadev/Timed-Quiz"
@@ -26,6 +29,7 @@ function Projects(){
 
      {
        name:"GIPHY API Assignment",
+       preview: "./assets/preview_images/GIPHY.png",
        description: "A page that uses the GIPHY API and JavaScript.",
        link: "https://smalagadev.github.io/GIPHY-API-Assignment",
        github: "https://github.com/smalagadev/GIPHY-API-Assignment"
@@ -33,6 +37,7 @@ function Projects(){
 
      {
         name:"SoundBars",
+        preview: "./assets/preview_images/SoundBars.png",
         description: "An artist previewing site utilizing Spotify and Youtube\"s API. A collaborated team effort focusing on javascript, jquery, and API usage.",
         link: "https://mahinjiwani.github.io/Project1/",
         github: "https://github.com/mahinjiwani/Project1"
@@ -40,6 +45,7 @@ function Projects(){
 
      {
        name:"LIRI BOT",
+       preview: "./assets/preview_images/LIRI.png",
        description: "LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data. There is no site link.",
        link: false,
        github: "https://github.com/smalagadev/LIRI"
@@ -54,5 +60,3 @@ function Projects(){
     </>
   );
 }
-
-export default Projects;
